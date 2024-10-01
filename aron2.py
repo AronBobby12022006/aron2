c6d9e364ba52b704d4ad7d2f91d8e091dfb13d3b
AronBobby12022006 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-10-01 16:01:41 +0530

    Add files via upload
    programme header added

--- aron2.py
+++ aron2.py
@@ -1,4 +1,6 @@
-name=input("enter the name")
-number=int(input("enter the number of times u wanted to print the name"))
+#AUTHOR:Aron Bobby Daniel
+#DATE:1-02-2024
+name=input("enter your name")
+number=int(input("enter the number of times you want to print your name"))
 result=name*number
 print(result)
